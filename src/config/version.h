0a4f4167e015597a87fbd09e755a305312c1c5d6
Андрей Епифанов <epifanovmd@gmail.com>
2025-11-20 11:33:44 +0300

    refactoring

---
+++ src/config/version.h
@@ -0,0 +1,11 @@
+// version.h
+#ifndef VERSION_H
+#define VERSION_H
+
+#define FIRMWARE_VERSION "1.0.1"
+
+// Дополнительная информация о сборке
+#define COMPILE_DATE __DATE__
+#define COMPILE_TIME __TIME__
+
+#endif // VERSION_H
