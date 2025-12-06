bb9124c6e18e6af38539fc5d20df466bfbfcecd2
Андрей Епифанов <epifanovmd@gmail.com>
2025-12-06 12:54:22 +0300

    refactoring

--- src/common/Version.h
+++ src/common/Version.h
@@ -2,10 +2,10 @@
 #define FIRMWARE_VERSION_H
 
 // Автоматически сгенерированная версия
-// Сгенерировано: 2025-12-06 12:50:59
-#define FIRMWARE_VERSION "1.0.9"
+// Сгенерировано: 2025-12-06 12:53:51
+#define FIRMWARE_VERSION "1.0.1"
 #define FIRMWARE_BUILD_DATE "2025-12-06"
-#define FIRMWARE_BUILD_TIME "12:50:59"
-#define FIRMWARE_BUILD_UNIX 1765014659
+#define FIRMWARE_BUILD_TIME "12:53:51"
+#define FIRMWARE_BUILD_UNIX 1765014831
 
 #endif
